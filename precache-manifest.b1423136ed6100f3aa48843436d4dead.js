e45435622ab9f66754a6b62fdb4f46854a1002ac
Galib77 <razagalib7@gmail.com>
2020-09-14 10:03:23 +0530

    Updates

---
+++ precache-manifest.b1423136ed6100f3aa48843436d4dead.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e7d49fa95787f69ff55b31df29181f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2ad2b310b0a22701968",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "4d5a2cf4eedd32d70db4",
+    "url": "/static/css/main.0ce7b30b.chunk.css"
+  },
+  {
+    "revision": "e2ad2b310b0a22701968",
+    "url": "/static/js/2.f09430fd.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.f09430fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5a2cf4eedd32d70db4",
+    "url": "/static/js/main.295aeb06.chunk.js"
+  },
+  {
+    "revision": "0566311b38c23f62f8b7",
+    "url": "/static/js/runtime-main.618c94da.js"
+  },
+  {
+    "revision": "7221b950227892b242dc8f3dafc86a2d",
+    "url": "/static/media/banner.7221b950.jpg"
+  },
+  {
+    "revision": "37863612d4a4af4d848b39dda77b61ac",
+    "url": "/static/media/colorful.37863612.png"
+  },
+  {
+    "revision": "2f01ad59fb0e6e945a7c1eea3b647923",
+    "url": "/static/media/home-about.2f01ad59.jpeg"
+  },
+  {
+    "revision": "b2d08e24d20bdde06524b36e35dc9a66",
+    "url": "/static/media/led-bulb-banner.b2d08e24.jpg"
+  },
+  {
+    "revision": "45de5a5a1cb5a9edf9074a63a6df70b2",
+    "url": "/static/media/led.45de5a5a.jpg"
+  },
+  {
+    "revision": "baed90c2f2809535388f35b0e99ea7de",
+    "url": "/static/media/rechargeable.baed90c2.jpg"
+  }
+]);
